302bb200fe54f32e776b3bbd547b53dfad131f4a
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-06-20 17:19:32 -0700

    Refactor validation into a seperate file

--- watcher.go
+++ watcher.go
@@ -7,7 +7,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
+
 	"sync"
 	"time"
 
@@ -36,57 +36,30 @@ type Watcher struct {
 	FolderFormat string   `json:"folder_format"`
 	Metadata     []Backup `json:"metadata"`
 
-	latestFileActivity time.Time
-	mu                 sync.Mutex
-	saveMetadataLock   sync.Mutex
-	fsnotifyWatcher    *fsnotify.Watcher
-	customObservers    []BackupCompleteObserver
-	stopChan           chan struct{}
-	backupRequestChan  chan struct{}
+	mu                sync.Mutex
+	fsnotifyWatcher   *fsnotify.Watcher
+	customObservers   []BackupCompleteObserver
+	stopChan          chan struct{}
+	backupRequestChan chan struct{}
 }
-
-var ErrorInvalidNameV2 = fmt.Errorf("error validating name")
-var ErrorInvalidWaitTime = fmt.Errorf("error validating wait time")
-var ErrorInvalidSource = fmt.Errorf("error validating source")
-var ErrorInvalidDestination = fmt.Errorf("error validating destination")
-var ErrorInvalidFolderFormat = fmt.Errorf("error validating folder format")
 
 func NewWatcher(name, source, destination string, waitTime float64, folderFormat string, enabled bool) (*Watcher, error) {
 	var errs error
-	// Validate name
-	if name == "" {
-		err := fmt.Errorf("%w: name cannot be empty", ErrorInvalidNameV2)
-		errs = errors.Join(errs, err)
-	}
-
-	// Validate wait time
-	if waitTime <= 0 {
-		err := fmt.Errorf("%w: wait time must be at least 0 seconds", ErrorInvalidWaitTime)
-		errs = errors.Join(errs, err)
-	}
-
-	if err := validateSourceAndDestination(source, destination); err != nil {
-		// These errors already include the "error validating"" prefix because it
-		// validates multiple values at once.
-		errs = errors.Join(errs, err)
-	}
-
-	if err := validateFolderFormat(waitTime, folderFormat); err != nil {
-		err := fmt.Errorf("%w: %s", ErrorInvalidFolderFormat, err)
-		errs = errors.Join(errs, err)
-	}
+	validateName(name, &errs)
+	validateWaitTime(waitTime, &errs)
+	validateFolderFormat(waitTime, folderFormat, &errs)
+	validateSourceAndDestination(source, destination, &errs)
 
 	w := &Watcher{
-		Name:               name,
-		Source:             source,
-		Destination:        destination,
-		Enabled:            enabled,
-		WaitTime:           waitTime,
-		FolderFormat:       folderFormat,
-		Metadata:           []Backup{},
-		latestFileActivity: time.Now(),
-		stopChan:           make(chan struct{}),
-		backupRequestChan:  make(chan struct{}, 1),
+		Name:              name,
+		Source:            source,
+		Destination:       destination,
+		Enabled:           enabled,
+		WaitTime:          waitTime,
+		FolderFormat:      folderFormat,
+		Metadata:          []Backup{},
+		stopChan:          make(chan struct{}),
+		backupRequestChan: make(chan struct{}, 1),
 	}
 
 	// Loading metadata relies on metadataJSONPath so it is easier to load the metadata
@@ -96,102 +69,6 @@ func NewWatcher(name, source, destination string, waitTime float64, folderFormat
 	}
 
 	return w, errs
-}
-
-// Validate the folder format.
-// Make sure that file names cannot overlap.
-// Make sure the format is supported by the filesystem.
-func validateFolderFormat(waitTime float64, folderFormat string) error {
-	var errs error
-
-	// Attempt to create two different times exactly one waitTime apart and make sure
-	// that the names are different to avoid potential collisions
-	seconds := int64(waitTime)
-	nanoseconds := int64((waitTime - float64(seconds)) * 1e9)
-	format1 := time.Unix(0, 0).Format(folderFormat)
-	format2 := time.Unix(seconds, nanoseconds).Format(folderFormat)
-	if format1 == format2 {
-		err := fmt.Errorf("folder format lacks adequate precision for wait time")
-		errs = errors.Join(errs, err)
-	}
-
-	return errors.Join(errs, validateDir(folderFormat, ErrorInvalidFolderFormat))
-}
-
-// Validate a path is a directory.
-// The path must be supported by the filesystem.
-// The path must not be a file.
-// If the path does not exist, it will be created.
-func validateDir(path string, invalidNameError error) error {
-	var errs error
-	var pathErr *os.PathError
-
-	info, err := os.Stat(path)
-
-	// errors.As(err, &pathErr) returns true if the file does not exist, so it must be
-	// checked after checking if the file exists
-	// os.IsNotExist(err) returns false if the name is invalid so it can be checked
-	// before checking if the name is invalid
-	if os.IsNotExist(err) {
-		if err := os.MkdirAll(path, 0755); err != nil {
-			errs = errors.Join(errs, err)
-		}
-	} else if errors.As(err, &pathErr) {
-		errs = errors.Join(errs, fmt.Errorf("%w: invalid name: %w", invalidNameError, err))
-	} else if err == nil && !info.IsDir() {
-		errs = errors.Join(errs, fmt.Errorf("%w: %s exists but is not a directory", invalidNameError, path))
-	} else if err != nil {
-		errs = errors.Join(errs, fmt.Errorf("%w: %w", invalidNameError, err))
-	}
-
-	return errs
-}
-
-// Validate source and destination directories.
-// The values rely on one another so both must be validated at the same time.
-// The paths must be supported by the filesystem.
-// The paths must not be a file.
-// If the paths do not exist, they will be created.
-// The paths must not be the same.
-// The destination must not be inside the source.
-func validateSourceAndDestination(source string, destination string) error {
-	var errs error
-
-	// Generic directory validation
-	errs = errors.Join(errs, validateDir(source, ErrorInvalidSource))
-	errs = errors.Join(errs, validateDir(destination, ErrorInvalidDestination))
-
-	// Get absolute paths so validation cannot be bypassed by using relative paths
-	absSource, err := filepath.Abs(source)
-	if err != nil {
-		errs = errors.Join(errs, fmt.Errorf("%w: error getting absolute path: %w", ErrorInvalidSource, err))
-	}
-	absDest, err := filepath.Abs(destination)
-	if err != nil {
-		err = fmt.Errorf("%w: error getting absolute path: %w", ErrorInvalidDestination, err)
-		errs = errors.Join(errs, err)
-	}
-
-	// Make sure source and destination are different
-	if absSource == absDest {
-		err = fmt.Errorf("%w: source and destination paths cannot be the same", ErrorInvalidSource)
-		errs = errors.Join(errs, err)
-		err = fmt.Errorf("%w: destination and source paths cannot be the same", ErrorInvalidDestination)
-		errs = errors.Join(errs, err)
-	}
-
-	// Make sure destination is not inside of source
-	relPath, err := filepath.Rel(absSource, absDest)
-	if err != nil {
-		err := fmt.Errorf("%w: error checking relative path from source to destination: %w", ErrorInvalidDestination, err)
-		errs = errors.Join(errs, err)
-	}
-	if !filepath.IsAbs(relPath) && !strings.HasPrefix(relPath, "..") && relPath != "." {
-		err := fmt.Errorf("%w: destination path cannot be inside the source path", ErrorInvalidDestination)
-		errs = errors.Join(errs, err)
-	}
-
-	return errs
 }
 
 func (w *Watcher) metadataJSONPath() string {
@@ -219,10 +96,6 @@ func (w *Watcher) loadMetadata() error {
 }
 
 func (w *Watcher) saveMetadata() error {
-	// TODO: What happens if metadata is a folder?
-	w.saveMetadataLock.Lock()
-	defer w.saveMetadataLock.Unlock()
-
 	data, err := json.MarshalIndent(w.Metadata, "", "  ")
 	if err != nil {
 		return fmt.Errorf("error marshaling metadata: %w", err)
@@ -411,9 +284,6 @@ func (w *Watcher) createBackup() {
 func (w *Watcher) handleFileEvent(event fsnotify.Event) {
 	// Add more information to Printf from the event
 	log.Printf("%s: File event detected: %s, Op: %s", w.Name, event.Name, event.Op)
-	w.mu.Lock()
-	w.latestFileActivity = time.Now()
-	w.mu.Unlock()
 	w.backupRequestChan <- struct{}{}
 }
 
